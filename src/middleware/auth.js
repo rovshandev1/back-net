a1cc5dc712eea8bf3f234b330c347f92b5487ea1
Ravshan <oravshan26@gmail.com>
2023-09-02 16:06:40 +0500

    ok

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 
 exports.authenticateUser = async (req, res, next) => {
   const token = req.header("Authorization");
